490097c107fa1caa048a6a1641d032e37f824654
moeaoun <moe_aoun@hotmail.com>
2025-03-21 19:56:36 +0200

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -5,6 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n -= 1  # Decrease n to ensure the loop terminates
     return result
 
 f = factorial(int(sys.argv[1]))
